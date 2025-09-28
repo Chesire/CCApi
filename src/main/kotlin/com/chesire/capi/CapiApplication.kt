659d4564079c7be78377a686733eb9374de7f25d
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-09-28 09:52:46 +0100

    Initial commit

---
+++ src/main/kotlin/com/chesire/capi/CapiApplication.kt
@@ -0,0 +1,11 @@
+package com.chesire.capi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/chesire/capi/CapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chesire.capi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
