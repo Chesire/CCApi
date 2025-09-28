b197cfab1a8bbd5c66f2bfb158d0af640fc564df
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-09-28 11:27:53 +0100

    Tidy up the challenge end points a little bit

--- src/main/kotlin/com/chesire/capi/controller/ChallengeController.kt
+++ src/main/kotlin/com/chesire/capi/controller/ChallengeController.kt
@@ -8,30 +8,32 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-// Questions to answer:
-// Should we get the user token by header or query param?
 @RestController
 @RequestMapping("/api/challenge")
 class ChallengeController {
 
-    @GetMapping
-    fun getChallenges(): String {
-        // TODO: Need to get all challenges for a single user, return as a JSON array.
-        return "Challenges requested"
+    @GetMapping("/{userId}")
+    fun getChallenges(@PathVariable userId: Long): ResponseEntity<String> {
+        // TODO: Need to get all user challenges by their id, return as a JSON array.
+        return ResponseEntity.notFound().build()
     }
 
-    @GetMapping("/{id}")
-    fun getChallenge(@PathVariable id: Long): ResponseEntity<String> {
+    @GetMapping("/{challengeId}")
+    fun getChallenge(@PathVariable challengeId: Long): ResponseEntity<String> {
         // TODO: Need to get a single challenge by its ID, return as a JSON object.
         return ResponseEntity.notFound().build()
     }
 
+    // Questions to answer:
+    // Should we get the user token by header or query param?
     @PostMapping
     fun addChallenge(@PathVariable data: Any): String {
         // TODO: Need to add a challenge for a user, return the created challenge as a JSON object.
         return "Challenge add $data requested"
     }
 
+    // Questions to answer:
+    // Should we get the user token by header or query param?
     @DeleteMapping("/{id}")
     fun deleteChallenge(@PathVariable id: Long): ResponseEntity<Void> {
         // TODO: Need to delete a challenge for a user.
