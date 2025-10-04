468ff568543976ff669e465308350e975f9c23c1
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-10-04 17:04:42 +0100

    Add annotation to validate the path params

--- src/main/kotlin/com/chesire/capi/challenge/ChallengeController.kt
+++ src/main/kotlin/com/chesire/capi/challenge/ChallengeController.kt
@@ -10,6 +10,7 @@ import com.chesire.capi.challenge.service.PostChallengeResult
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/challenges")
 class ChallengeController(private val challengeService: ChallengeService) {
