7ea6d475d6fd6c26cefe27e6e6f6197650a842d6
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-10-13 19:17:59 +0100

    Add the filter for the JWT

---
+++ src/main/kotlin/com/chesire/capi/config/JwtAuthenticationFilter.kt
@@ -0,0 +1,45 @@
+package com.chesire.capi.config
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(
+    private val jwtService: JwtService
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+
+        val authHeader = request.getHeader("Authorization")
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val jwt = authHeader.substring(7) // Remove "Bearer " prefix
+        val userId = jwtService.extractUserId(jwt)
+
+        if (userId != null && SecurityContextHolder.getContext().authentication == null) {
+            if (jwtService.isTokenValid(jwt)) {
+                val authToken = UsernamePasswordAuthenticationToken(
+                    userId,
+                    null,
+                    emptyList()
+                )
+                authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = authToken
+            }
+        }
+
+        filterChain.doFilter(request, response)
+    }
+}
